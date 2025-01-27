1794aa7a33dd9fa1fd4b77d9aad77f478dab007e
Chanok Sillapa <chanoksillapa@Books-MacBook-Pro.local>
2025-01-27 23:20:51 +0700

    fixing bug

--- customerCRUD/src/main/java/com/example/customerCRUD/controller/CustomerController.java
+++ customerCRUD/src/main/java/com/example/customerCRUD/controller/CustomerController.java
@@ -14,7 +14,7 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    @GetMapping("/all/in")
+    @GetMapping("/all")
     public String listCustomers(Model model) {
         model.addAttribute("customers", customerService.getAllCustomers());
         return "customer_list";
